6ba041e107d610bb583800462854a2c18ff7f881
M4-dev <hnbsoft@outlook.com>
2025-08-17 11:49:09 +0800

    HNB: Release v25.0817 version

--- HnbAllFiles/HnbMacOSCodes.swift
+++ HnbAllFiles/HnbMacOSCodes.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 /// Internal use, used to see whether you are using the correct version of HnbMixpanel framework.
-fileprivate let kHnbMixpanelFrameworkInternalVersion: String = "40.25.0815.2"
+fileprivate let kHnbMixpanelFrameworkInternalVersion: String = "40.25.0817.3"
 
 /// class HnbMixpanelInfo
 public class HnbMixpanelInfo: NSObject
